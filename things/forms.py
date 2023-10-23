173ce6026735772afec625892b8cbec029e07236
Gabriela Sevelkova <gsevelkova5@gmail.com>
2023-10-23 12:26:07 +0100

    Add Things form to forms.py

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,12 @@
+from .models import Thing
+from django import forms
 """Forms of the project."""
 
 # Create your forms here.
+class ThingForm(forms.ModelForm):
+    class meta: 
+        model = Thing
+        fields = ['name', 'description','quantity']
+        widgets = {'description':forms.Textarea()}
+        quantity = forms.NumberInput()
+        
