2e330f0dc56cc462475fb3d017a6d9baea5d2f6b
Gabriela Sevelkova <gsevelkova5@gmail.com>
2023-12-05 16:10:44 +0000

    Correct typo in Meta

--- things/forms.py
+++ things/forms.py
@@ -4,24 +4,22 @@ from django.db import models
 """Forms of the project."""
 
 # Create your forms here.
-class ThingForm(forms.Form):
-    #class meta: 
-        # model = Thing
-        # fields = ['name', 'description', 'quantity']
-        # widgets = {'description':forms.Textarea()}
-        #quantity = forms.NumberInput()
-    first_name = forms.CharField(label='name')
-    description = forms.CharField(label='description')
-    quantity = forms.NumberInput(label='quantity')
+class ThingForm(forms.ModelForm):
+    class Meta: 
+        model = Thing
+        exclude = ['created_at']
+        fields = ['name', 'description', 'quantity']
+        widgets = {'description':forms.Textarea()}
+    
 
 
     
     def save(self):
         super().save(commit=False)
         thing = models.Model.objects.create(
-            name=self.cleaned_data.get('name'),
-            description=self.cleaned_data.get('description'),
-            quantity=self.cleaned_data.get('quantity'),
+        name=self.cleaned_data.get('name'),
+        description=self.cleaned_data.get('description'),
+        quantity=self.cleaned_data.get('quantity'),
 
         )
         return thing
--- things/models.py
+++ things/models.py
@@ -1,6 +1,7 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
+
 class Thing(models.Model):
     name = models.CharField(max_length=35, unique=True)
     description = models.CharField(max_length=120, blank=True)
